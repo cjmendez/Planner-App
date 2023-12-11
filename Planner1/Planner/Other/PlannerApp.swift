ee7e772bf3d42cbb67a82ff5d383b15d41c5664b
cjmendez <70356772+cjmendez@users.noreply.github.com>
2023-12-10 17:58:17 -0800

    CURRENT

--- Planner1/Planner/Other/PlannerApp.swift
+++ Planner1/Planner/Other/PlannerApp.swift
@@ -9,22 +9,34 @@ import SwiftUI
 
 @main
 struct PlannerApp: App {
-    init(){
+    init() {
         FirebaseApp.configure()
     }
+    
     var body: some Scene {
         WindowGroup {
-            InitialView()
+            TabView {
+                InitialView()
+                    .tabItem {
+                        Text("Initial")
+                    }
+                
+                ContentView()
+                    .modelContainer(for: Task.self)
+                    .tabItem {
+                        Text("Task Management")
+                    }
+            }
         }
     }
 }
 
-struct Task_ManagementApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(for: Task.self)
-    }
-}
+//struct Task_ManagementApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//        .modelContainer(for: Task.self)
+//    }
+//}
 
